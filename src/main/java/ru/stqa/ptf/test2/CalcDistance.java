fb409d414f5431908f79ceb929d83ab2a1ee640f
s.krivenko <s.krivenko@qiwi.com>
2019-11-04 21:47:35 +0300

    создан запускаемый класс

---
+++ src/main/java/ru/stqa/ptf/test2/CalcDistance.java
@@ -0,0 +1,15 @@
+package ru.stqa.ptf.test2;
+
+public class CalcDistance {
+
+    public static void main(String[] args) {
+        Point p1 = new Point(4,7);
+        Point p2 = new Point(0,12);
+
+        System.out.println("Расстояние между точками" + " = " + distance(p1,p2));
+    }
+
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt(((p2.x - p1.x) * (p2.x - p1.y))) + ((p2.y - p1.y) * (p2.y - p1.y));
+    }
+}
