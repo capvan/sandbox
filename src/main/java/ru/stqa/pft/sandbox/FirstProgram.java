ca0b39cedfb3c67e096b43c2509948fdbf304b87
s.krivenko <s.krivenko@qiwi.com>
2019-11-04 20:44:44 +0300

    Добавление конструкторов

--- src/main/java/ru/stqa/pft/sandbox/FirstProgram.java
+++ src/main/java/ru/stqa/pft/sandbox/FirstProgram.java
@@ -3,15 +3,18 @@ package ru.stqa.pft.sandbox;
 public class FirstProgram {
 
     public static void main(String[] args) {
-       hello("world");
+        hello("world");
         hello("user");
 
-        double l = 5;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+        //создание нового объекта
+        Square s = new Square(5);
+        //заполнение атрибутов объекта
 
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+        double l = 5;
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+
+        Rectangle r = new Rectangle(4,6);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
 //        int l = 6;
 //        int s = l * l;
 //        System.out.println("Площадь квадрата со стороной " + l + " = " + s);
@@ -21,11 +24,11 @@ public class FirstProgram {
         System.out.println("Hello " + somebody + "!");
     }
 
-    public static double area(double len) {
-        return len * len;
+    public static double area(Square s) {
+        return s.l * s.l;
     }
 
-    public static double area(double a, double b) {
-        return a * b;
+    public static double area(Rectangle r) {
+        return r.a * r.b;
     }
 }
---
+++ src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,11 @@
+package ru.stqa.pft.sandbox;
+
+public class Square {
+    //атрибуты
+    public double l;
+
+    public Square(double l) {
+        //инициализация нового объекта
+        this.l = l;
+    }
+}
