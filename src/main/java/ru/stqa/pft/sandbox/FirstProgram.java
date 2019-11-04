4603c2ad85a8de81cde4fab28a343f8f6feee31d
s.krivenko <s.krivenko@qiwi.com>
2019-11-04 19:59:18 +0300

    Проверка математических выражений

--- src/main/java/ru/stqa/pft/sandbox/FirstProgram.java
+++ src/main/java/ru/stqa/pft/sandbox/FirstProgram.java
@@ -2,7 +2,26 @@ package ru.stqa.pft.sandbox;
 
 public class FirstProgram {
 
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        System.out.println(2 + 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+        System.out.println(2 - 2);
+        System.out.println(1 / 2);
+        System.out.println(1.0 / 2);
+        System.out.println(1 / 2.0);
+        System.out.println(2.0 / 2);
+
+        System.out.println("2" + "2");
+        System.out.println("2" + 2);
+
+        System.out.println(2 + 2 * 2);
+        System.out.println("2 + 2 = " + (2 + 2));
+
+        int l = 6;
+        int s = l * l;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+    }
 }
